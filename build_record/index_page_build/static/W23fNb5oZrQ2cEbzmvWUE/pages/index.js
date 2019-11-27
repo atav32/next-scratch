3daba185edfa1e4871b9ea21ae399bc1cedf50a5
Brian Zhang <brianbzhang@gmail.com>
2019-11-27 09:47:20 -0800

    recording simple next builds

---
+++ build_record/index_page_build/static/W23fNb5oZrQ2cEbzmvWUE/pages/index.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RNiq:function(n,t,e){"use strict";e.r(t);var i=e("q1tI"),u=e.n(i).a.createElement;t.default=function(){return u("div",null,u("p",null,"Hello Next.js"))}},vlRD:function(n,t,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return e("RNiq")}])}},[["vlRD",0,1]]]);
