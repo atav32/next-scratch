809ab38cbaa86ef9edd177925136100f89301b2e
Brian Zhang <brianbzhang@gmail.com>
2020-03-09 14:20:26 -0700

    fix indent

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 const Index = () => (
   <div>
-		<Link href="/about">
-			<a>About Page</a>
-		</Link>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
