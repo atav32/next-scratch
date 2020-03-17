343916fcc0ea3326c113bd6b87d0ee84cae3b03d
Brian Zhang <brianbzhang@gmail.com>
2020-03-17 16:35:06 -0700

    add polyfill.io script tag

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 const Index = () => (
   <div>
+    <Head>
+      <script type="text/javascript" src="https://polyfill.io/v3/polyfill.min.js?features=fetch%2CPromise%2CURL%2CUint8Array%2CObject.assign"></script>
+    </Head>
     <Link href="/about">
       <a>About Page</a>
     </Link>
