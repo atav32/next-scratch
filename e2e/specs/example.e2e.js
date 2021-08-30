4d958674b71752c77f4a106a0467dace15b115e2
BZ (Brian Zhang) <bz@stripe.com>
2021-08-30 07:59:50 -0700

    wdio config

---
+++ e2e/specs/example.e2e.js
@@ -0,0 +1,15 @@
+import LoginPage from  '../page_objects/login.page';
+import SecurePage from '../page_objects/secure.page';
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
